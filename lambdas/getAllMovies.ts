bc67107b64041212344a4936c9ccd538cab1934a
Denis <marincasdenis@yahoo.com>
2024-10-08 00:51:52 +0100

    Lambda function to get all movies.

---
+++ lambdas/getAllMovies.ts
@@ -0,0 +1,56 @@
+import { Handler } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+// Initialization
+const ddbDocClient = createDDbDocClient();
+
+// Handler
+export const handler: Handler = async (event, context) => {
+  try {
+    console.log("Event: ", JSON.stringify(event));
+
+    // Using ScanCommand to get all movies from table
+    const commandOutput = await ddbDocClient.send(
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+      })
+    );
+
+    const body = {
+      data: commandOutput.Items || [],
+    };
+
+    // Return Response
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
